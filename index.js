7aeb97a1391237b5c35f525fd2490f37e08c78cc
Emon <emonhasan7650@gmail.com>
2025-02-01 10:43:23 +0600

    web socket connect to client

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
+io.on("connection", (socket) => {
+    console.log("New user has connected", socket.id);
+})
 
 const port = 9000;
 
-server.listen(port, ()=> console.log(`Server Start at ${port}`)
-);
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+    return res.sendFile('/public/index.html')
+})
+
+server.listen(port, () => console.log(`Server Start at Port: ${port}`));
